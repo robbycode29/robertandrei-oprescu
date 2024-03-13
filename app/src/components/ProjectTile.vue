9f7a162870b942279a3fb4f7d3b35ee52f6eb04f
Robert <robertandrei.oprescu@gmail.com>
2024-03-13 16:57:49 +0200

    added projects page

---
+++ app/src/components/ProjectTile.vue
@@ -0,0 +1,43 @@
+<template>
+    <div @click="goTo(project.url)" class="bg-white tile-shadow rounded-xl min-w-72 min-h-72 cursor-pointer">
+        <div class="flex flex-col h-full">
+            <div class="flex-1">
+                <img :src="project.image" alt="Project image"
+                    class="rounded-t-xl w-full h-36 object-cover" />
+            </div>
+            <div class="flex-1 p-2">
+                <h1 class="text-sm font-semibold">{{ project.name }}</h1>
+                <span class="text-xs font-normal">{{ project.description }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'ProjectTile',
+    props: {
+        project: {
+            type: Object,
+            required: true,
+        },
+    },
+    methods: {
+        goTo(url: string) {
+            window.open(url, '_blank');
+        },
+    },
+});
+</script>
+<style scoped>
+.tile-shadow {
+    box-shadow:
+        2.8px 2.8px 2.8px -65px rgba(0, 0, 0, 0.02),
+        6.7px 6.7px 6.7px -65px rgba(0, 0, 0, 0.028),
+        12.5px 12.5px 12.5px -65px rgba(0, 0, 0, 0.035),
+        22.3px 22.3px 22.3px -65px rgba(0, 0, 0, 0.042),
+        41.8px 41.8px 41.8px -65px rgba(0, 0, 0, 0.05),
+        100px 100px 100px -65px rgba(0, 0, 0, 0.07);
+}
+</style>
--- app/src/components/ProjectsPage.vue
+++ app/src/components/ProjectsPage.vue
@@ -1,10 +1,72 @@
 <template>
-    <div>Projects</div>
+    <div
+        class="flex flex-col gap-6 w-full p-4 m-8 ml-24 rounded-xl background-page overflow-y-scroll scrollbar-thin adjusted-height">
+        <h1 class="text-xl font-semibold text-center self-center">Personal projects</h1>
+        <span class="text-sm font-normal self-center">These are a few projects that I love sharing. They are no start-up
+            ideas 🚀, just a few things to display my coding capabilities. 💻</span>
+        <div class="h-[1.5px] bg-black w-full rounded-xl"></div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center items-center">
+            <div v-for="project in projects" :key="project.id">
+                <ProjectTile :project="project" />
+            </div>
+        </div>
+    </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ProjectTile from './ProjectTile.vue';
+import projects from '@/assets/projects.json';
 
 export default defineComponent({
-  name: 'ProjectsPage',
+    name: 'ProjectsPage',
+    components: {
+        ProjectTile,
+    },
+    data() {
+        return {
+            boxes: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        };
+    },
+    computed: {
+        projects() {
+            return projects.map(project => ({
+                ...project,
+                image: require('@/assets/' + project.image)
+            }));
+        },
+    },
 });
 </script>
+<style scoped>
+.background-page {
+    background-color: rgba(255, 255, 255, 0.53);
+}
+
+.scrollbar-thin::-webkit-scrollbar {
+    width: 13px;
+}
+
+.scrollbar-thin::-webkit-scrollbar-track {
+    background: transparent;
+    border-radius: 10px;
+}
+
+.scrollbar-thin::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 10px;
+    height: calc(100% - 20px);
+    border: 5px solid rgba(255, 255, 255, 0.9);
+}
+
+/* .scrollbar-thin::-webkit-scrollbar-thumb:hover {
+    background: #555;
+} */
+
+body {
+    scrollbar-width: thin;
+}
+
+.adjusted-height {
+    height: calc(100vh - 140px);
+}
+</style>
