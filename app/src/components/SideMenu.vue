e885ae743d4675ef6db98d5550b59ef60786a90d
Robert Oprescu <robert.oprescu_ext@sonepar.com>
2024-11-29 11:25:06 +0200

    small fix ui 1.1.0

--- app/src/components/SideMenu.vue
+++ app/src/components/SideMenu.vue
@@ -18,7 +18,7 @@
             <span class="text-gray-800 font-medium group-hover:flex">Info</span>
         </button>
     </div>
-    <div v-if="$route.path == '/'" class="absolute bottom-0 w-full bg-[#cce3ff] shadow-xl px-5 py-5 lg:hidden flex flex-row justify-center gap-12">
+    <div v-if="$route.path == '/'" class="fixed bottom-0 w-full bg-[#cce3ff] shadow-xl px-5 py-5 lg:hidden flex flex-row justify-center gap-12">
         <button @click="changeMenu(0)" class="flex flex-row gap-6">
             <img src="../assets/wand.svg" class="w-6 h-6" />
         </button>
@@ -32,7 +32,7 @@
             <img src="../assets/phone.svg" class="w-6 h-6" />
         </button>
     </div>
-    <div v-else-if="$route.path == '/contact'" class="absolute bottom-0 w-full bg-[#cce3ff] shadow-xl px-5 py-5 lg:hidden flex flex-row justify-center gap-12">
+    <div v-else-if="$route.path == '/contact'" class="fixed bottom-0 w-full bg-[#cce3ff] shadow-xl px-5 py-5 lg:hidden flex flex-row justify-center gap-12">
         <button @click="$router.push('/')" class="flex flex-row items-center gap-6">
             <img src="../assets/user.svg" class="w-5 h-5" />
         </button>
